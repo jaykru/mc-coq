540b186b826a9f2057825abe69d540eaf305274e
Jay Kruer <j@dank.systems>
2023-09-06 21:16:26 -0700

    finished bmc correctness proof for always props

--- mc2.v
+++ mc2.v
@@ -9,8 +9,6 @@ Axiom forall_exists_duality1 : forall {T} (P: T -> Prop), ~ (exists x, P x) <-> 
 Axiom forall_exists_duality2 : forall {T} (P: T -> Prop), ~ (forall x, P x) <-> (exists x, ~ P x).
 Axiom DeMorgan1 : forall (P Q : Prop), ~(P /\ Q) <-> (~P \/ ~Q).
 Axiom DeMorgan2 : forall (P Q : Prop), ~(P \/ Q) <-> (~P /\ ~Q).
-Axiom TODO : False.
-
 Ltac classical :=
   repeat match goal with
   | [ H : context[_ -> False] |- _ ] => rewrite (fold_not) in H (* sometimes intuition leaves us with negations like this. *)
@@ -62,35 +60,6 @@ Section LTLdef.
         True
       end
   where "ζ ⊨ φ" := (models ζ φ).
-
-  (* Fixpoint models_upto (k:nat) (ζ : TemInt) (φ : LTL): Prop := *)
-  (*     match k with *)
-  (*     | 0 => (* base case: modeling something for 0 cycles is vaccuously  *)
-  (*               false. this is the only way to make until and next work out. *) *)
-  (*            False *)
-  (*     | (S k') => match φ with *)
-  (*             | injp_ltl p => *)
-  (*               p (ζ 0) (* to model an immediate property for the next S k' cycles, *)
-  (*                          it suffices to satisfy it right now. *) *)
-  (*             | imp_ltl ψ π => *)
-  (*               (models_upto k ζ ψ) -> (models_upto k ζ π) *)
-  (*             | next_ltl ψ => *)
-  (*               (* next ψ says that ψ will be true in the next cycle. *)
-  (*                  for this to be true for up to k cycles, *)
-  (*                  we need that ψ is maintained for up to k-1 cycles by the suffix of ζ *) *)
-  (*               models_upto k' (ζ ^^ 1) ψ *)
-  (*             | until_ltl ψ π => *)
-  (*               exists (i:nat), *)
-  (*                 (i <= k) *)
-  (*                 /\ models_upto (k-i) (ζ ^^ i) π (* note that this *doesn't* mean *)
-  (*                                                    that π need be true for k-i cycles. *) *)
-  (*                 /\ (forall j, j < i -> ζ ^^ j ⊨ ψ) *)
-  (*             | false_ltl => *)
-  (*               False *)
-  (*             | true_ltl => *)
-  (*               True *)
-  (*       end *)
-  (*     end. *)
 
   Fixpoint clarke_bm_helper (i k:nat) (ζ : TemInt) (φ : LTL): Prop :=
       match φ with
@@ -232,74 +201,65 @@ End LTLdef.
 Record Mealy : Type := mkMealy {
                            Σ : Type
                          ; Q : Type
-                         ; δ : Q -> Σ -> Q * Σ
+                         ; δ : Q -> Σ -> Q
                          ; Q0 : Q
                          }.
 
-Fixpoint MealyTrace' (M : Mealy) (I : nat -> Σ M) (n : nat) {struct n}: (Q M) * option (Σ M) :=
+Fixpoint MealyTrace' (M : Mealy) (I : nat -> Σ M) (n : nat) {struct n}: (Q M) :=
     match n with
-    | 0 => ((Q0 M) , None)
-    | S n' => let (Q', _) := MealyTrace' M I n' in
-             match (δ M) Q' (I n') with
-             | (q,σ) => (q, Some σ)
-             end
+    | 0 => Q0 M
+    | S n' => let q' := MealyTrace' M I n' in (δ M) q' (I n')
     end.
 
-Definition MealyTrace (M : Mealy) (I : nat -> Σ M) : @TemInt (Q M * option (Σ M)) :=
+Definition MealyTrace (M : Mealy) (I : nat -> Σ M) : @TemInt (Q M) :=
     fun n => MealyTrace' M I n.
 
 Definition models_upto M k φ :=
       (* M models φ upto k cycles if (clake_bm k ζ φ) holds for all input traces I *)
       forall I, clarke_bm k (MealyTrace M I) φ.
 
-Lemma MealyTrace0isQ0: forall M I, fst (MealyTrace M I 0) = Q0 M.
+Lemma MealyTrace0isQ0: forall M I, MealyTrace M I 0 = Q0 M.
   auto.
 Defined.
 
 Search pair.
 
-Definition state_seq (M: Mealy) : Type :=
-    { s : nat -> Q M
-      & { n : nat
-          & ((forall t, (0 < t /\ t < n) ->
-                  { input : Σ M & s t = fst ((δ M) (s (t - 1)) input) } ) * (s 0 = Q0 M))%type } }.
+Record state_seq (M: Mealy) : Type := {
+    states : nat -> Q M;
+    inputs : nat -> Σ M;
+    len : nat;
+    start_ok : (states 0 = Q0 M);
+    connected: (forall t, (0 < t /\ t < len) -> (δ M) (states (t - 1)) (inputs (t-1)) = states t);
+  }.
 
-Definition len {M} (s : state_seq M) : nat := ltac:(repeat destruct s;
-                                                  match goal with
-                                                  | [ n : nat |- _ ] => exact n
-                                                  end).
+Arguments states {M}.
+Arguments inputs {M}.
+Arguments len {M}.
+Arguments start_ok {M}.
+Arguments connected {M}.
 
-Definition state_fn {M} (s : state_seq M) : nat -> Q M := ltac:(repeat destruct s;
-                                                       match goal with | [f : nat -> Q M |- _] => exact f
-                                                       end).
-
+Lemma mealy_unfold_step : forall M I t,
+    t > 0 ->
+    (MealyTrace M I t) = δ M (MealyTrace M I (t - 1)) (I (t - 1)).
+  intros.
+  destruct t.
+  { lia. }
+  {
+    simpl.
+    unshelve erewrite (_ : forall k, k - 0 = k). { lia. }
+    auto.
+  }
+Defined.
 
 Definition reachability_diameter (M : Mealy) : Type :=
   (* A reachability diameter D is a natural number such that every state reachable
      in n steps can also be reached in D <= n steps. *)
   { D : nat &
-  forall (s : state_seq M) (n: nat) (_ : len s = n) (end_Q : Q M) (_ : (state_fn s) n = end_Q),
-    (D <= n /\ exists (s' : state_seq M ), len s' = D /\ (state_fn s') D = end_Q) }.
-
-Lemma repeats_past_rd : forall M I b D (_D: reachability_diameter M),
-    D = projT1 _D ->
-    b > D ->
-    (exists k, (k >= 0 /\ k <= D) -> MealyTrace M I k = MealyTrace M I b).
-  Proof.
-    intros M I b D _D HD b_gt_D.
-    destruct _D.
-    simpl in *.
-    symmetry in HD.
-    subst.
-    unshelve epose proof (_ : { s : state_seq M & (forall q : (Q M), exists (t : nat), t <= len s /\ (state_fn s) t = q)} ).
-    {
-      admit.
-    }
-    destruct X as [s pf].
-    Abort. (* I don't think this is actually true, because it can take less time to reach a reachable state than to repeat a state *)
+        forall (s : state_seq M) (end_Q : Q M) (_ : s.(states) (s.(len) - 1) = end_Q),
+          (D > 0 /\ D <= s.(len) /\ exists (s' : state_seq M ), len s' = D /\ s'.(states) (D-1) = end_Q) }.
 
 Theorem rd_sufficies_bmc_gp_case:
-  forall (D: nat) (M: Mealy) (I: nat -> Σ M) (p: (Q M * option (Σ M)) -> Prop) (_D : reachability_diameter M),
+  forall (D: nat) (M: Mealy) (I: nat -> Σ M) (p: Q M -> Prop) (_D : reachability_diameter M),
     D = projT1 _D ->
     models_upto M D (always_ltl (injp_ltl p)) ->
     models (MealyTrace M I) (always_ltl (injp_ltl p)).
@@ -349,183 +309,73 @@ Proof.
     solve[lia].
     clear H.
 
-    epose (q := fst (MealyTrace M I x)).
-    (* constructing the always q state sequence *)
-    unshelve epose proof ( _ : { s : state_seq M & (state_fn s (len s) = q /\ len(s) = x)}) as X.
+    epose (q := MealyTrace M I x).
+    (* constructing the MealyTrace M I state sequence *)
+    unshelve epose proof ( _ : { s : state_seq M & (s.(states) (len s - 1) = q /\ s.(len) = (x+1)) }) as MI_seq.
     {
       unshelve econstructor.
       { unshelve econstructor.
-        { exact (fun t => fst (MealyTrace M I t)). }
-        { unshelve econstructor; try solve[exact x].
-          split; [|auto].
-          unshelve econstructor.
-          exact (I (t-1)).
-          Set Nested Proofs Allowed.
-          Lemma mealy_unfold_step : forall M I t,
-              t > 0 ->
-              fst (MealyTrace M I t) = fst (δ M (fst (MealyTrace M I (t - 1))) (I (t - 1))).
-            intros.
-            destruct t.
-            { lia. }
-            {
-              simpl.
-              unshelve erewrite (_ : forall k, k - 0 = k). { lia. }
-              unshelve destruct (MealyTrace M I t).
-              simpl.
-              Lemma fst_stupid: forall A B (p : A * B),
-                  fst (let (a,b) := p in (a, Some b)) = fst p.
-                intros.
-                unfold fst.
-                destruct p.
-                auto.
-              Defined.
-
-              rewrite fst_stupid.
-              reflexivity.
-            }
-          Defined.
-          rewrite mealy_unfold_step.
-          auto.
-          lia.
-        }
-      }
-      { simpl; auto. }
-    }
-    (* Now just need to use X... *)
-    destruct X as [s [Hs H_len_s]].
-    specialize (Drd s (len s) ltac:(auto) q Hs).
-    destruct Drd as [HDlens seq_wit].
-    destruct seq_wit as [s' [len_s'_is_D s'_ends_with_q]].
-    unfold state_seq in s'.
-    destruct s' as [s'fn [len_s' [s'connected s'start_ok]]]; simpl in *.
-
-    Definition state_seq_to_temint {M} (s: state_seq M) (s_len_2plus: len s >= 2): nat -> Σ M.
-      destruct s as [s_fn [len_s [s_connected s_start_ok]]].
-      intro x.
-      remember x as n.
-      destruct x.
-      { (* x = 0 *)
-        simpl in *.
-        destruct (s_connected 1 ltac:(lia)) as [input input_good].
-        exact input.
-      }
-      {
-        simpl in *.
-        destruct (PeanoNat.Nat.ltb (S n) len_s).
-        { (* x + 1 < len_s *)
-          specialize (s_connected 1 ltac:(lia)) as [input input_good].
-          exact input.
+        { exact (fun t => (MealyTrace M I t)). }
+        {
+          exact I.
         }
         {
-          (* x+1 > len_s, don't care so we use s_connected 1.*)
-          destruct (s_connected 1 ltac:(lia)) as [input input_good].
-          exact input.
+          exact (x+1).
+        }
+        {
+          simpl.
+          auto.
+        }
+        {
+          simpl.
+          intros.
+          symmetry.
+          unshelve rewrite mealy_unfold_step at 1.
+          auto.
+          intuition auto.
         }
       }
-    Defined.
-
-    Lemma state_seq_to_temint_ok: forall {M},
-        forall (t: nat),
-          (forall (q: Q M) (s : state_seq M) (s_len_2plus: len s >= 2),
-              t <= len s ->
-              (state_fn s) t = q ->
-              fst (MealyTrace M (state_seq_to_temint s s_len_2plus) t) = q).
-    Proof.
-      induction t; intros ? ? ?; destruct s as [s_fn [s_len [s_connected s_start_ok]]].
       {
-        simpl in *.
-        intros.
-        subst.
+        simpl.
+        unshelve erewrite (_ : x+1-1 = x). lia.
         auto.
       }
-      {
-        intros H1 H2.
-        cbn in H1,H2.
-        cbn -[state_seq_to_temint] in IHt.
-        rewrite mealy_unfold_step.
-        unshelve erewrite (_ : S t - 1 = t) in *. solve[lia].
-        unshelve erewrite IHt.
-        all: admit.
-      }
-      Admitted.
-
-    pose (state_seq_to_temint s ltac:(case TODO)) as I'.
-    pose proof (state_seq_to_temint_ok x q s ltac:(case TODO)) as I'ok.
+    }
+    (* From MI_seq ending in q, we use that D is a reachability diameter to get
+       a new state sequence of length D ending also in q. *)
+    destruct MI_seq as [s [Hs H_len_s]].
+    specialize (Drd s q Hs).
+    destruct Drd as [Dnz [HDlens seq_wit]].
+    destruct seq_wit as [s' [len_s'_is_D s'_ends_with_q]].
+    (* remember s' as s''. *)
+    (* destruct s' as [s'states s'inputs s'len s'start_ok s'connected]; simpl in *. *)
 
 
-
-    unshelve epose proof (_ : { I' : nat -> Σ M & fst (MealyTrace M I' x) = q } ) as I'.
+    assert (forall t', t' < s'.(len) -> MealyTrace M s'.(inputs) t' = s'.(states) t') as H.
     {
-      (* defining I'(t) = if t <= D then i'_t else i'_D *)
-      unshelve econstructor.
+      induction t'.
       {
-          intro t.
-          remember (PeanoNat.Nat.ltb t D) as pf.
-          destruct pf.
-          {
-            (* t < D case *)
-            Search Nat.leb.
-            assert (H_t_lt_D: t < D).
-            {
-              eapply PeanoNat.Nat.ltb_lt;
-              symmetry in Heqpf;
-              assumption.
-            }
-
-            remember (PeanoNat.Nat.eqb t 0) as pf2.
-            destruct pf2.
-            {
-              (* t == 0 case*)
-              exact (I 0). (* this doesn't actually matter, so I just use a random alphabet symbol I had in context.*)
-            }
-            subst.
-            assert (H_t_ne_0: t <> 0).
-            {
-              symmetry in Heqpf2.
-              eapply PeanoNat.Nat.eqb_neq.
-              symmetry in Heqpf;
-              assumption.
-            }
-            assert (H_t_gt_0: t > 0). { solve[lia]. }
-            unshelve epose proof (s'connected t _) as H_exists_input.
-            lia.
-            destruct H_exists_input as [input _].
-            exact input.
-          }
-          {
-            (* t >= D case, just use i'_D *)
-            unshelve epose proof (s'connected (len_s' - 1) ltac:(case TODO)) as i'_D_and_pf.
-            destruct i'_D_and_pf as [i'_D _].
-            exact i'_D.
-          }
-      }
-      {
-        match goal with
-          | [|- fst ?x = _ ] => destruct x
-        end.
         simpl.
+        symmetry.
+        exact s'.(start_ok).
+      }
+      {
+        intro.
+        rewrite mealy_unfold_step.
+        unshelve erewrite (_ : S t' - 1 = t').
+        lia.
+        rewrite IHt'.
+        unshelve erewrite (_ : t' = S t' - 1) at 1 2. lia.
+
+        erewrite s'.(connected).
+        auto.
+        all: lia.
       }
     }
-          {
-            specialize (HBoundedModel I' x).
-            assert (fst (MealyTrace M I' x) = fst (MealyTrace M I x)).
-            {
-              simpl.
-              destruct (MealyTrace M I' x), (MealyTrace M I x), (δ M q0 (I' x)), (δ M q0 (I x)).
-              simpl.
-              rewrite IHx.
-              r
-                auto.
-
-              induction x; simpl; [auto|].
-              cbv in IHx.
-              simpl.
-              rewrite IHx.
-              auto.
-              simpl. }
-          }
-    }
-
-    ltac:(case TODO).
+    change (p q).
+    rewrite <-s'_ends_with_q.
+    erewrite <-H.
+    eapply HBoundedModel.
+    all: subst; lia.
   }
-  Defined.
+Qed.
